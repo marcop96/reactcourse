f075c15d34bd8f949c96d019943d30f9375cc344
marcop96 <55959162+marcop96@users.noreply.github.com>
2023-08-10 20:46:32 +0200

    adds new components to create new expenses

---
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -0,0 +1,26 @@
+import "./expenseForm.css";
+
+function ExpenseForm() {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" />
+        </div>
+
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" />
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default ExpenseForm;
---
+++ src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,11 @@
+import "./NewExpense.css";
+import "./ExpenseForm";
+function NewExpense() {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+}
+
+export default NewExpense;
