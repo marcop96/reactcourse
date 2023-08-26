780dfa78e52631c05ba96ddad317620fc3782be3
Marco Pianaroli <marco.pianaroli96@gmail.com>
2023-08-26 10:33:14 +0200

    fixes another case sensitive directory error

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-import "./expenseForm.css";
+import "./ExpenseForm.css";
 
 function ExpenseForm(props) {
   const [enteredTitle, setEnteredTitle] = useState("");
