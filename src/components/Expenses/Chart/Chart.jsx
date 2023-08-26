7803a16b02a36a67e618ef18673dc107772e77ee
Marco Pianaroli <marco.pianaroli96@gmail.com>
2023-08-26 10:10:04 +0200

    removes consolelogs

--- src/components/Expenses/Chart/Chart.jsx
+++ src/components/Expenses/Chart/Chart.jsx
@@ -2,10 +2,8 @@
 import ChartBar from "./ChartBar";
 import "./Chart.css";
 function Chart(props) {
-  console.log(props.dataPoints);
   const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
   const totalMaximum = Math.max(...dataPointValues);
-  console.log(props);
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
--- src/components/Expenses/Chart/ChartBar.jsx
+++ src/components/Expenses/Chart/ChartBar.jsx
@@ -3,10 +3,10 @@ import "./chartBar.css";
 
 function ChartBar(props) {
   let barFillHeight = "0%";
+  console.log(props.value);
   if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
-  console.log(barFillHeight);
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
